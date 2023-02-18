e44b0222a4e9c35c8d9deff3b889622843fa1fd8
hopeVaughn <hopevaughnwarren@gmail.com>
2023-02-18 11:37:49 -0800

    add has_sum template

--- memoization/can_sum.js
+++ memoization/can_sum.js
@@ -1,10 +1,7 @@
 /*
 Write a function 'canSum(targetSum, numbers)' that takes in a targetSum and an array of numbers as arguments.
-
 The function should return a boolean indicating whether or not it is possible to generate the targetSum using the numbers from the array.
-
 You may use an element of the array as many times as needed.
-
 You may assume that all input numbers are nonnegative
 */
 
---
+++ memoization/how_sum.js
@@ -0,0 +1,14 @@
+/*
+Write a function 'howSum(targetSum, numbers)' that takes in a targetSum and an array of numbers as arguments.
+
+The function should return an array containing any combination of elements that add up to exactly the targetSum. If there is no combination that adds up tp the targetSum, then return null.
+*/
+
+function howSum(targetSum, numbers) {
+  // do something
+}
+
+console.log(howSum()) //
+console.log(howSum()) //
+console.log(howSum()) //
+console.log(howSum()) //
