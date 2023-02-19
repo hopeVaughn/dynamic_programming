a63e099ccc87daf802076e09a348ab22696cc8c9
hopeVaughn <hopevaughnwarren@gmail.com>
2023-02-19 12:16:43 -0800

    add and finish can_construct challenge

---
+++ memoization/can_construct.js
@@ -0,0 +1,41 @@
+/*
+Write a function 'canConstruct(target, wordBank)' that accepts a target string and an array of strings.
+
+The function should return a boolean indicating whether or not the 'target' can be constructed by concatenating elements of the 'wordBank' array.
+
+You may reuse the elements of the 'wordBank' as many times as needed.
+*/
+
+function canConstruct(target, wordBank, memo = {}) {
+  if (target in memo) return memo[target];
+  if (target === '') return true;
+
+  for (let word of wordBank) {
+    if (target.indexOf(word) === 0) {
+      const suffix = target.slice(word.length);
+      if (canConstruct(suffix, wordBank, memo) === true) {
+        memo[target] = true;
+        return true;
+      }
+    }
+  }
+  memo[target] = false;
+  return false;
+}
+
+console.log(canConstruct("abcdef", ['ab', 'abc', 'cd', 'def', 'abcd'])); // true
+console.log(canConstruct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar'])); // false
+console.log(canConstruct('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't'])); // true
+console.log(canConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', ['e', 'ee', 'eee', 'eeee', 'eeeee', 'eeeeee'])); // false
+
+// m = target.length
+// n = wordBank.length
+
+// space and time complexity w/o memoization.
+// O(n^m*m) time - Branching factor to the height factor
+// O(m^2) space
+
+
+// space and time complexity w/ memoization
+// O(n*m^2) time
+// O(m^2) space
